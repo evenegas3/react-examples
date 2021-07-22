0962d85deda52183f4417606a5f2aa65f2089765
evenegas3 <evenegas3@users.noreply.github.com>
2021-07-21 21:03:39 -0700

    init BasicModal Component

---
+++ modal-test-project/src/BasicModal.jsx
@@ -0,0 +1,13 @@
+//rafce
+
+import React from 'react'
+
+const BasicModal = () => {
+    return (
+        <div>
+            <p>Hello from BasicModal.jsx</p>
+        </div>
+    )
+}
+
+export default BasicModal
