134e8b5d0489b93b53b8d6512da7eeb6bf3dbedd
evenegas3 <42683810+evenegas3@users.noreply.github.com>
2021-08-03 20:22:40 -0700

    created basic forms

--- modal-test-project/src/components/Modals/ButtonModal.jsx
+++ modal-test-project/src/components/Modals/ButtonModal.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 function ButtonModal() {
 
+  const { register } = useForm();
+
     return (
       <div>
-          <p>test</p>
+          <p>Please Fill Out The Following User Information</p>
+          <form>
+            <input {...register("firstName")} placeholder="First name" />
+            <input {...register("lastName")} placeholder="Last name" />
+            <button type="submit">Submit</button>
+          </form>
       </div>
     );
   }
